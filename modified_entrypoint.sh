bdd798d601adeb39f4a267c984dae6d50ff29e73
Rohan Sukhdeo <33191632+rohansukhdeo@users.noreply.github.com>
2022-12-30 15:49:56 -0500

    Update modified_entrypoint.sh

--- modified_entrypoint.sh
+++ modified_entrypoint.sh
@@ -13,15 +13,16 @@ esac
 # Check for inactivity every minute
 while true; do
     # Check the last input time on the sway display
-    last_input_time=$(wlr-input-inhibitor -d $WAYLAND_DISPLAY)
+#    last_input_time=$(wlr-input-inhibitor -d $WAYLAND_DISPLAY)
     
     # If the last input time is more than 15 minutes ago, shut down the container
-    if [ $(( $(date +%s) - $last_input_time )) -gt 900 ]; then
+#    if [ $(( $(date +%s) - $last_input_time )) -gt 900 ]; then
 
 # Check the idle time on the sway display
-#    idle_time=$(swayidle -d $WAYLAND_DISPLAY -t 900 get)
+    idle_time=$(swayidle -d $WAYLAND_DISPLAY -t 900 get)
+
 # If the idle time is greater than or equal to 15 minutes, shut down the container
-#     if [ "$idle_time" -ge 900 ]; then
+    if [ "$idle_time" -ge 900 ]; then
         exit 0
     fi
     
