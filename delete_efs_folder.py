1d7e997245d23ec6756f02146e75d15575a8f9a6
Rohan Sukhdeo <33191632+rohansukhdeo@users.noreply.github.com>
2023-01-28 12:20:53 -0500

    Update delete_efs_folder.py

--- delete_efs_folder.py
+++ delete_efs_folder.py
@@ -1,16 +1,27 @@
+import os
+import subprocess
 import boto3
 
-def lambda_handler(event, context):
+def delete_folder(event, context):
     # Create a client for the EFS service
     efs_client = boto3.client('efs')
-    
-    # Get the ID of the EFS file system you want to delete a folder from
-    file_system_id = 'your-efs-file-system-id'
-    
-    # Get the path of the folder you want to delete
-    folder_path = '/path/to/folder'
-    
-    # Use the EFS client to delete the folder
-    efs_client.delete_mount_target(FileSystemId=file_system_id, MountTargetId=folder_path)
-    print(f'Folder {folder_path} deleted from EFS file system {file_system_id}')
 
+    # Get the EFS file system ID and folder path from the event
+    file_system_id = event['file_system_id']
+    folder_path = event['folder_path']
+
+    # Get the DNS name for the EFS file system
+    file_system = efs_client.describe_file_systems(FileSystemId=file_system_id)
+    dns_name = file_system['FileSystems'][0]['Name']
+
+    # Mount the EFS file system to the local file system
+    mount_command = f"mount -t nfs4 -o nfsvers=4.1 {dns_name}:/ {folder_path}"
+    subprocess.run(mount_command, shell=True)
+
+    # Delete the folder using the rm command
+    delete_command = f"rm -rf {folder_path}/*"
+    subprocess.run(delete_command, shell=True)
+
+    # Unmount the EFS file system from the local file system
+    unmount_command = f"umount {folder_path}"
+    subprocess.run(unmount_command, shell=True)
